52985c179f26c2a11523f5f89e9642778ae5f8ab
Yaroslav Anaiko <anaikoyaroslav8@gmail.com>
2025-10-08 10:16:23 +0300

    splitted code to files,added comments,improved readebility

---
+++ random_picker.py
@@ -0,0 +1,19 @@
+# Importing necessary modules
+
+import Genre_list, random
+
+
+def get_random_movie_by_genre(genre_name):
+
+    # Getting a list of movies for the specified genre
+
+    movies = Genre_list.search_movies_by_genre(genre_name)
+
+    # If no movies found, return None
+
+    if not movies:
+        return None
+    # Returning a random movie from the list
+
+    return random.choice(movies)
+
